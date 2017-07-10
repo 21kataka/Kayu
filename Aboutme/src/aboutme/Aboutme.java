0b43d26f69da623dcbfb4282e631bd89848f64ad
Kayu <21kataka@canacad.ac.jp>
2017-07-10 10:34:06 -0700

    aboutme projet

---
+++ Aboutme/src/aboutme/Aboutme.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aboutme;
+
+/**
+ *
+ * @author Education Unlmited
+ */
+public class Aboutme {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("My name is Kayu");
+        System.out.println("I am 14 years old");
+        System.out.println("I am from Japan"); 
+    }
+    
+}
---
+++ Person/src/person/Person.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author Education Unlmited
+ */
+public class Person {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int age = 14;
+       String country = "Japan";
+       String firstname = "Kayu";
+       String lastname = "Katagiri";
+       boolean isastudent = true;
+       
+       System.out.println("My name is " + firstname + "" + lastname);
+       System.out.println("I am from " + country + " and I am " + age + " years old");
+        
+    }
+    
+}
